eec14015e02afabdc2b75169a5d1ee00f91c4569
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-10-06 11:23:37 +0200

    feat(sso): improve sso datas rights mapping

--- src/main/java/fr/openent/flashquizz/service/impl/DefaultFlashquizzSsoService.java
+++ src/main/java/fr/openent/flashquizz/service/impl/DefaultFlashquizzSsoService.java
@@ -4,6 +4,7 @@ import fr.openent.flashquizz.config.FlashquizzConfig;
 import fr.openent.flashquizz.service.FlashquizzSsoService;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -38,6 +39,7 @@ public class DefaultFlashquizzSsoService implements FlashquizzSsoService {
         JsonObject params = new JsonObject()
                 .put(USER_ID, userId)
                 .put("workflowPrefix", config.workflowPrefix());
+
         Neo4j.getInstance().execute(query, params, Neo4jResult.validUniqueResultHandler(result -> {
             if (result.isLeft()) {
                 String error = String.format(
@@ -49,7 +51,20 @@ public class DefaultFlashquizzSsoService implements FlashquizzSsoService {
             }
 
             JsonObject ssoData = result.right().getValue();
-            promise.complete(ssoData);
+            JsonArray workflowRights = ssoData.getJsonArray("workflowRights", new JsonArray());
+
+            JsonObject enrichedData = new JsonObject()
+                    .put("id", ssoData.getString("id"))
+                    .put("login", ssoData.getString("login"))
+                    .put("displayName", ssoData.getString("displayName"))
+                    .put("email", ssoData.getString("email"))
+                    .put("profile", ssoData.getString("profile"))
+                    .put("hasQuizzView", workflowRights.contains("quizz.view"))
+                    .put("hasQuizzGestion", workflowRights.contains("quizz.gestion"))
+                    .put("hasGameView", workflowRights.contains("game.view"))
+                    .put("hasGameGestion", workflowRights.contains("game.gestion"));
+
+            promise.complete(enrichedData);
         }));
 
         return promise.future();
