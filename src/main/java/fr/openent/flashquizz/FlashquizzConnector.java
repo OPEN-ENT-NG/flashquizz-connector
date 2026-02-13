4341d466dd15a2e0c9ccbf1004cba6fa27968734
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-02-13 09:19:06 +0100

    feat: #RBACK-117, kubernetes compatibility

--- src/main/java/fr/openent/flashquizz/FlashquizzConnector.java
+++ src/main/java/fr/openent/flashquizz/FlashquizzConnector.java
@@ -4,6 +4,7 @@ import fr.openent.flashquizz.config.FlashquizzConfig;
 import fr.openent.flashquizz.controller.FlashquizzController;
 import fr.openent.flashquizz.service.ServiceFactory;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.neo4j.Neo4j;
@@ -14,16 +15,19 @@ public class FlashquizzConnector extends BaseServer {
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
-        super.start(startPromise);
-        
+        final Promise<Void> promise = Promise.promise();
+        super.start(promise);
+        promise.future()
+            .compose(e -> initFlashquizz())
+            .onFailure(th -> log.error("[Flashquizz-Connector@FlashquizzConnector::start] Failed to start Flashquizz module.", th))
+            .onComplete(startPromise);
+    }
+
+    public Future<Void> initFlashquizz() {
         FlashquizzConfig flashquizzConfig = new FlashquizzConfig(config);
         this.serviceFactory = new ServiceFactory(vertx, Neo4j.getInstance(), flashquizzConfig);
-        
-        addController(new FlashquizzController(serviceFactory));
-        
-        startPromise.tryComplete();
-        startPromise.tryFail("[Flashquizz-Connector@FlashquizzConnector::start] Failed to start Flashquizz module.");
-        
-        vertx.deployVerticle(SsoFlashquizz.class, new DeploymentOptions().setConfig(config));
+        return addController(new FlashquizzController(serviceFactory))
+            .compose(e -> vertx.deployVerticle(SsoFlashquizz.class, new DeploymentOptions().setConfig(config)))
+            .mapEmpty();
     }
 }
